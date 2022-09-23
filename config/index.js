5d89f008f91258e0d4257885d138560d25eb6eec
Yuanhang Ying <77554299+Edyyh@users.noreply.github.com>
2022-09-23 16:08:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oVOpB6f5a2CKGhrV3isEdWa_t7Eo",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "MzZsV9TVg-Y7mnBvf0YCF-zrsEaF_S715D1DUo1zEoI",
+      useTemplateId: "snCNkiB5hQHS-voau5Dzo-JA3MOqkvcM4iLh3CY9KHI",
       // 所在省份
       province: "江苏",
       // 所在城市
