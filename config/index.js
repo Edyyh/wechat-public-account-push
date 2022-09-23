8b09948b765c05ec31429168b80088cdc85f347d
Yuanhang Ying <77554299+Edyyh@users.noreply.github.com>
2022-09-23 16:04:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -74,22 +74,22 @@ export const USER_CONFIG = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://s.weibo.com/top/summary/",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1998", "date": "06-27"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        //{"type": "*生日", "name": "老婆", "year": "1998", "date": "06-27"},
+        {"type": "节日", "name": "恋爱纪念日", "year": "2021", "date": "04-17"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        {"type": "生日", "name": "呱呱🍉", "year": "1998", "date": "06-27"},
+        {"type": "节日", "name": "叽叽🐤", "year": "1997", "date": "09-06"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
         {"keyword": "love_day", date: "2021-04-17"},
-        {"keyword": "birthday", date: "1998-06-27"},
-        {"keyword": "my_birthday", date: "1997-09-16"},
+        {"keyword": "birthday", date: "2023-06-27"},
+        {"keyword": "my_birthday", date: "2023-09-06"},
         // 结婚纪念日
         //{"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
